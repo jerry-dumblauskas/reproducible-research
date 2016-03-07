2e822e6e7efbacc9745a37b052b122253c2fa657
jerry-dumblauskas <jdumblauskas@gmail.com>
2016-03-06 21:46:20 -0600

    homework scratch

---
+++ hw_scratch.R
@@ -0,0 +1,5 @@
+x<-read.csv("repdata-data-StormData.csv.bz2")
+y<-c('EVTYPE','FATALITIES', 'INJURIES', 'PROPDMG', 'PROPDMGEXP', 'CROPDMG', 'CROPDMGEXP')
+zz<-subset(x,select=y)
+unique(zz$PROPDMGEXP)
+zz[zz$PROPDMGEXP=='-',]
